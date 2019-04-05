99ee601e352ba45d57c069b7a84f9858352a8af4
oslobird <andrey940@gmail.com>
2019-04-05 14:04:55 +0300

    desktop layout + mixins + contacts form logic

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@ $('a[href^="#"]').on('click', function (event) {
   if ($target.length) {
     event.preventDefault();
     $('html, body').stop().animate({
-      scrollTop: $target.offset().top-5
+      scrollTop: $target.offset().top
     }, 500, 'easeInOutQuad');
   }
 });
@@ -21,4 +21,65 @@ $('.navigation-mobile').on('click', function () {
 $('.navigation-desktop a').on('click', function () {
   $('.navigation-mobile').removeClass('open');
   $('.navigation-desktop').removeClass('collapse');
+});
+
+
+// FORM LOGIC
+
+let valideteForm = function (formValue) {
+  let namePattern = /^[A-Za-z]+$/;
+  let mailPattern = /^[a-z0-9]\w+\.?\w*@[a-z]+\.[a-z]{2,8}$/;
+
+  if (namePattern.test(formValue.name)) {
+    $('#formname').css('border-bottom', '1px solid #0052FF');
+  } else {
+    $('#formname').css('border-bottom', '1px solid red');
+    return false
+  }
+
+  if (mailPattern.test(formValue.email)) {
+    $('#formemail').css('border-bottom', '1px solid #0052FF');
+  } else {
+    $('#formemail').css('border-bottom', '1px solid red');
+    return false
+  }
+
+  if (formValue.message == '') {
+    $('#formmessage').css('border-bottom', '1px solid red');
+    return false
+  } else {
+    $('#formmessage').css('border-bottom', '1px solid #0052FF');
+  }
+
+  if (formValue.termsAndConditions) {
+    $('.form-radio-box').removeClass('errorCheckbox');
+  } else {
+    $('.form-radio-box').addClass('errorCheckbox');
+    return false
+  }
+  
+  return true;
+}.bind(this);
+
+$("#submit-button").click(function (event) {
+
+  let formValue = $(this.form).serializeArray().reduce(function (obj, item) {
+    obj[item.name] = item.value;
+    return obj;
+  }, {});
+
+  if (valideteForm(formValue)) {
+    $(".contact-content-form__text").text("Thank you! We will we will get back to you as soon as possible ⚡️");
+    $(".contact-content-form__text").addClass('success');
+    // $.ajax({
+    //   url: "",
+    //   method: "POST",
+    //   data: formValue,
+    //   success: function (data) {
+    //     $("#formsubmit").text("Thank you!");
+    //   }
+    // });
+  };
+
+  event.preventDefault();
 });
